112767ee5f9f54c69f47776f27d0702a9fcd9b23
Waqas Tahir <wakaztahir@gmail.com>
2023-10-02 15:05:34 +0500

    timeline page added

---
+++ src/solid/icons/timeline/GooglePlayIcon.tsx
@@ -0,0 +1,19 @@
+const GooglePlayIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" style={{width : "48px", height : "48px"}} viewBox="0 0 35.022 39.987">
+            <g id="icons8-google-play" transform="translate(-7 -4.008)">
+                <path id="Path_8" data-name="Path 8"
+                      d="M7.7,4.043A1,1,0,0,0,7,5.121v37.97a.835.835,0,0,0,.5.856L27.681,23.883Z" fill="#4db6ac"/>
+                <path id="Path_9" data-name="Path 9"
+                      d="M33.237,18.36l-8.307-4.8L8.789,4.244a1.454,1.454,0,0,0-1.084-.2l19.977,19.84Z" fill="#dce775"/>
+                <path id="Path_10" data-name="Path 10"
+                      d="M8.417,43.8,33.282,29.445l-5.6-5.562L7.5,43.947A1.089,1.089,0,0,0,8.417,43.8Z" fill="#d32f2f"/>
+                <path id="Path_11" data-name="Path 11"
+                      d="M41.4,23.071c-.8-.429-8.1-4.676-8.1-4.676l-.061-.035-5.556,5.523,5.6,5.562,8.059-4.653C42.285,24.248,42.194,23.5,41.4,23.071Z"
+                      fill="#fbc02d"/>
+            </g>
+        </svg>
+    )
+}
+
+export default GooglePlayIcon
---
+++ src/solid/routes/timeline.tsx
@@ -0,0 +1,318 @@
+import {Anique, Column, Headline, Size} from "@qinetik/anique";
+import {styled} from "@qinetik/emotion";
+import WindowsIcon from "../icons/timeline/WindowsIcon.tsx";
+import WebIcon from "../icons/timeline/WebIcon.tsx";
+import GooglePlayIcon from "../icons/timeline/GooglePlayIcon.tsx";
+import LightDarkIcon from "../icons/LightDarkIcon.tsx";
+import SyncCircle from "../icons/timeline/SyncCircle.tsx";
+import CloudSyncIcon from "../icons/timeline/CloudSyncIcon.tsx";
+
+export default function TimelinePage() {
+
+    const theme = Anique
+
+    return (
+        <div>
+            <MainContainer>
+                <MainSlide>
+                    <Headline size={Size.Large} style={{color: theme.colors.onBg, "text-align" : "center", "margin-top" : "-1em"}}>
+                        Meet <span style={{background: theme.colors.accent}}>T</span>imeline
+                    </Headline>
+                    <Headline style={{color: theme.colors.onBg, "text-align" : "center", "margin-top" : "0"}}>
+                        A Note Taking Tool
+                    </Headline>
+                    <MainDemoContainer>
+                        <img
+                            src={"/apps/timeline/screenshots/demo-1.png"}
+                            alt={"Demo First"}
+                            width={1000}
+                        />
+                    </MainDemoContainer>
+                </MainSlide>
+            </MainContainer>
+            <ContentContainer>
+                <DemoSectionOne/>
+                <DemoSectionTwo/>
+                <AllFeatures/>
+                <AvailableSection/>
+                <div style={{height: "2em"}}/>
+            </ContentContainer>
+        </div>
+    )
+}
+
+const SecondaryHeading = styled("h2")`
+    ${Anique.breakpoints.down("md")} {
+        font-size: 2rem !important;
+    }
+`
+
+const MainHeading = styled("h1")`
+    ${props => Anique.breakpoints.down("md")} {
+        font-size: 4rem !important;
+    }
+`
+
+const CenteredColumn = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const MainSlide = styled(CenteredColumn)`
+    padding-top: 2em;
+    margin-bottom: 1em;
+
+    ${Anique.breakpoints.up("sm")} {
+        padding-top: 6em;
+    }
+
+    ${Anique.breakpoints.up("md")} {
+        padding-top: 14em;
+    }
+`
+
+const MainDemoContainer = styled("div")`
+    position: absolute;
+    top: 30em;
+    transform: translateY(-50%);
+
+    ${Anique.breakpoints.up("sm")} {
+        top: 40em;
+    }
+
+    ${Anique.breakpoints.up("md")} {
+        top: 60em;
+    }
+`
+
+const MainContainer = styled("div")`
+    width: 100%;
+    height: 30em;
+    position: relative;
+    background: ${Anique.colors.bg200};
+
+    ${Anique.breakpoints.up("sm")} {
+        height: 40em;
+    }
+
+    ${Anique.breakpoints.up("md")} {
+        height: 60em;
+    }
+`
+
+const ContentContainer = styled("div")`
+    margin-top: 30em;
+`
+
+const AllFeatures = styled((props) => {
+    return (
+        <div {...props}>
+            <FeatureBox>
+                <LightDarkIcon/>
+                <FeatureText>
+                    Light / Dark Theme
+                </FeatureText>
+            </FeatureBox>
+            <FeatureBox>
+                <CloudSyncIcon/>
+                <FeatureText>
+                    Cloud Auto Sync
+                </FeatureText>
+            </FeatureBox>
+            <FeatureBox>
+                <SyncCircle/>
+                <FeatureText>
+                    Local Backup
+                </FeatureText>
+            </FeatureBox>
+        </div>
+    )
+})`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-width: 60%;
+    max-width: 80%;
+    margin: 2em auto;
+
+    ${Anique.breakpoints.up("md")} {
+        flex-direction: row;
+        justify-content: space-between;
+    }
+`
+
+const AvailableSection = () => {
+
+    const theme = Anique
+
+    return (
+        <CenteredColumn>
+            <span style={{color: theme.colors.primary}}>
+                Available Now
+            </span>
+            <a href="https://qinetik.github.io/timeline-web" style={{"text-decoration": "none"}} target={"_blank"}>
+                <CustomMainButton style={{width: "270px"}}>
+                    <WebIcon/>
+                    <span style={{
+                        width: "100%",
+                        color: "white",
+                        "margin-left": "1em",
+                    }}>
+                        Try on Web
+                    </span>
+                </CustomMainButton>
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.wakaztahir.timeline"
+                  style={{"text-decoration": "none"}} target={"_blank"}>
+                <CustomMainButton style={{width: "270px"}}>
+                    <GooglePlayIcon/>
+                    <span style={{
+                        width: "100%",
+                        color: "white",
+                        "margin-left": "1em"
+                    }}>
+                    Download on Play Store
+                </span>
+                </CustomMainButton>
+            </a>
+            <a href="https://github.com/codeckle/timeline-kmp/releases/download/1.0.19/Timeline-1.0.19.msi"
+                  style={{"text-decoration": "none"}} target={"_blank"}>
+                <CustomMainButton style={{width: "270px"}}>
+                    <WindowsIcon/>
+                    <span style={{
+                        width: "100%",
+                        color: "white",
+                        "margin-left": "1em"
+                    }}>
+                    Download for Windows
+                </span>
+                </CustomMainButton>
+            </a>
+        </CenteredColumn>
+    )
+}
+
+const DemoSectionOne = () => {
+    return (
+        <DemoContainer>
+            <DemoText>
+                <h3 style={{ "text-align":"center" }}>
+                    Sketch Out <br/>
+                    Your Thoughts
+                </h3>
+                <br/>
+                <p style={{ "text-align":"center" }}>
+                    Timeline provides most features required to draw out multiple
+                    images into one note , that can be shared & exported. <br/>
+                    Features like undo , redo , background color change , pointer color ,
+                    zoom & pan and the ability to toggle between them easily. <br/>
+                    But most prominent feature of sketch is that it exports to SVG.
+                </p>
+            </DemoText>
+            <DemoImage>
+                <img
+                    src={"/apps/timeline/screenshots/demo-2.png"}
+                    alt={"Demo Second"}
+                    width={1000}
+                />
+            </DemoImage>
+        </DemoContainer>
+    )
+}
+
+const DemoSectionTwo = styled((props) => {
+    return (
+        <DemoContainer {...props}>
+            <DemoImage>
+                <img
+                    src={"/apps/timeline/screenshots/demo-3.png"}
+                    alt={"Demo Third"}
+                    width={1000}
+                />
+            </DemoImage>
+            <DemoText>
+                <h3 style={{ "text-align" : "center" }}>
+                    Intuitive <br/>
+                    Easy To Understand
+                </h3>
+                <br/>
+                <p style={{ "text-align": "center" }}>
+                    Timeline makes it easy to access the items you need.
+                    Everything will feel under your control.<br/>
+                    It will save you a low of time to do everything else and
+                    help you to be more productive , The app will suit to your needs.
+                </p>
+            </DemoText>
+        </DemoContainer>
+    )
+})`
+    flex-direction: column-reverse;
+
+    ${Anique.breakpoints.up("sm")} {
+        flex-direction: row;
+    }
+`
+
+export const FeatureBox = styled("div")`
+  width: 12em;
+  height: 2em;
+  border-radius: 1.5em;
+  padding : 1em;
+  margin : 1em;
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:center;
+  border : 2px solid ${Anique.colors.primary}; 
+`
+
+export const FeatureText = styled("span")`
+  font-family: 'Roboto', 'sans-serif';
+  padding-left : 1em;
+  color : ${Anique.colors.primary};
+`
+
+export const CustomMainButton = styled("button")`
+  border-radius:1.5em;
+  height:3.6em;
+  text-align:center;
+  text-decoration:none;
+  opacity:0.9;
+  padding:2em;
+  border:0;
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  cursor:pointer;
+  margin:0.5em;
+  background : ${Anique.colors.primary};
+  &:hover{
+    opacity:1;
+  }
+`
+
+export const DemoContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  padding: 1em 4em;
+  justify-content: center;
+  align-items: center;
+
+  ${Anique.breakpoints.up("sm")} {
+    flex-direction: row;
+  }
+`
+
+export const DemoText = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const DemoImage = styled("div")`
+
+`
